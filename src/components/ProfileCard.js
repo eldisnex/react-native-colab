e80ab1c45c394f34290e9ed414fa42c42023e4ab
Frantag15 <48178331@ort>
2025-08-13 10:03:32 -0300

    ProfileCard hecha

---
+++ src/components/ProfileCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+export default function ProfileCard({ name, avatar, bio }) {
+  return (
+    <View style={styles.card}>
+      <Image source={{ uri: avatar }} style={styles.avatar} />
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.bio}>{bio}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 15,
+    backgroundColor: '#CDFFD8',
+    borderRadius: 10,
+    alignItems: 'center',
+    marginVertical: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  avatar: { 
+    width: 80, 
+    height: 80, 
+    borderRadius: 40, 
+    marginBottom: 10 
+  },
+  name: { 
+    fontSize: 20, 
+    fontWeight: 'bold', 
+    color: '#333' 
+  },
+  bio: { 
+    fontSize: 14, 
+    color: '#555', 
+    textAlign: 'center',
+    marginTop: 4 
+  },
+});
