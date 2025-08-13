9fff4639eea1cee029db2b551a920b7bf5f71f38
Frantag15 <48178331@ort>
2025-08-13 10:08:46 -0300

    modificacion en greeting.js

--- src/component/Greeting.js
+++ src/component/Greeting.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Greeting() {
    return (
       <View>
-         <Text>Hello world!</Text>
+         <Text>Me gusta el mate</Text>
       </View>
    );
 }
